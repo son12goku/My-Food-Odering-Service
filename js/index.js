ae3546ed23e4e52ce79824ecaa5cb2aa3bbf7e06
Rishabh Updhyay (Son Goku) <son121goku@gmail.com>
2021-06-05 20:53:04 +0530

    Initial commiit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+//button for submit message in contact part 
+
+function showMessage() {
+    alert("Your message has been received ");
+}
+
+//button for submit order in home part 
+
+function showOrderMessage() {
+    alert("Your order request has been received ");
+}
+
+
+//button for link our page to home
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
